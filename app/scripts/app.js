f136952783836a062fb00e7da9ce730087aa8151
Chirag Shah <chiragshah321@gmail.com>
2017-02-28 01:41:09 +0000

    changes to app.js

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,28 +1,23 @@
 (function () {
-
-    function config ($stateProvider, $locationProvider) {
-
+    function config($stateProvider, $locationProvider) {
         $locationProvider
          .html5Mode({
-             "enabled": true,
-             "requireBase": false
+             'enabled': true,
+             'requireBase': false
          });
 
         $stateProvider
-         .state("home", {
-             "controller": "RoomCtrl as room",
-             "templateUrl": "/templates/home.html",
-             "url": "/"
+         .state('home', {
+             'controller': 'RoomCtrl as room',
+             'templateUrl': '/templates/home.html',
+             'url': '/'
          })
-         .state("room", {
-             "templateUrl": "/templates/room.html",
-             "url": "/room"
+         .state('room', {
+             'templateUrl': '/templates/room.html',
+             'url': '/room'
          });
-
     }
-
     angular /* global angular*/
-        .module("blocChat", ["ui.router", "ui.bootstrap", "firebase"])
+        .module('blocChat', ['ui.router', 'ui.bootstrap', 'firebase'])
         .config(config);
-
 })();
